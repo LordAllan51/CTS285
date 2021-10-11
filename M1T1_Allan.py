a1ba68c60b79fbc97387e045fb52af28b64a9916
LordAllan51 <74980052+LordAllan51@users.noreply.github.com>
2021-10-11 13:18:53 -0400

    Add files via upload

---
+++ M1T1_Allan.py
@@ -0,0 +1,10 @@
+# CTS 285
+# M1T1 - Orientation
+# Lord Jenar Adolph C Allan
+# 8/16/2021
+
+print("Lord Jenar Allan")
+print("Python, Java, Swift")
+print("My interest is to learn how to code, how to create code, break codes,",
+      "\nand develop an app of my design and also help design that was required by the customer.",
+      "\nMy hobbies are playing games, reading slow and fast mish-mash of novels.")
